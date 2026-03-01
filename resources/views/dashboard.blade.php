9ad8df91906bf65652522935e203af936d9651e2
Ngazid <sn_ngazid@outlook.com>
2026-03-01 15:03:35 +0300

    feat: design sur trois colonnes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,7 +14,7 @@
 
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
 
                 @forelse($accounts as $account)
 
