eeb1ec40eed936d73b025eb089607d863c22e29e
Ngazid <sn_ngazid@outlook.com>
2026-03-05 16:46:22 +0300

    feat: mettre favicon lakile

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
 <title>Lakile — Coffre-fort numérique d'entreprise</title>
 
 {{-- ⚡ CRITIQUE : doit être le PREMIER script, avant tout CSS --}}
