98a0678ac05c3940aac18d9079604760cd6913d2
Ngazid <sn_ngazid@outlook.com>
2026-03-10 09:42:37 +0300

    feat: favicon

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}"> -->
+
     <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+    <link rel="manifest" href="{{ asset('site.webmanifest') }}">
+    <meta name="theme-color" content="#4F7CFF">
+
     <title>Lakile</title>
 
     <!-- Fonts -->
@@ -27,7 +35,7 @@
 
         <div class="w-max mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
             <div style="position: relative; z-index: 1;">
-            {{ $slot }}
+                {{ $slot }}
             </div>
         </div>
     </div>
